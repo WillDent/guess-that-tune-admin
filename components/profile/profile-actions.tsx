cf867dcef054b8b6ffd3e9e21ed955501e2ef28d
Will Dent <will.dent@gmail.com>
2025-07-30 07:16:43 -0400

    Use EditProfileModal stub to fix missing profile prop error
    - Switch to edit-profile-modal-stub which doesn't require profile data
    - The full modal would need profile data passed from parent component

--- components/profile/profile-actions.tsx
+++ components/profile/profile-actions.tsx
@@ -3,7 +3,7 @@
 import { useState } from 'react'
 import { Button } from '@/components/ui/button'
 import { Settings, Edit } from 'lucide-react'
-import { EditProfileModal } from './edit-profile-modal'
+import { EditProfileModal } from './edit-profile-modal-stub'
 import Link from 'next/link'
 import { useAuth } from '@/lib/auth/client'
 
